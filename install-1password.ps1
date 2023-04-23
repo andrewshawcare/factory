d6f128f1958bdb2c15553f8b7585fcf3ed1e83d7
Andrew Shaw Care <andrewshawcare@gmail.com>
2023-04-23 15:17:31 -0400

    Add 1Password guard

--- install-1password.ps1
+++ install-1password.ps1
@@ -1,1 +1,9 @@
-﻿winget install --id AgileBits.1Password --silent
+﻿$packageId = "AgileBits.1Password"
+
+winget show --exact --id $packageId
+
+if ($?) {
+    Write-Host "${packageId} is already installed."
+} else {
+    winget install --exact --id $packageId --silent
+}
