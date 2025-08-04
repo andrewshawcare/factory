3c79103e48c9c5b7eddb70f18ddd288746110773
Andrew Shaw Care <andrewshawcare@gmail.com>
2025-08-04 10:50:36 -0400

    Add installation script for Elgato Wave Link

---
+++ install-elgato-wave-link.ps1
@@ -0,0 +1,9 @@
+$packageId = "Elgato.WaveLink"
+
+winget list --exact --id $packageId
+
+if ($?) {
+    Write-Host "${packageId} is already installed."
+} else {
+    winget install --exact --id $packageId --silent
+}
